b751a5e0e47105a4606f67cf63473531d3725821
ServerJon <ctsesiphon@hotmail.com>
2021-01-15 04:35:47 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cursoAmpliandoAngularOpenWebinars'`, () => {
+  it(`should have as title 'cursoAngularOpenWebinar'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cursoAmpliandoAngularOpenWebinars');
+    expect(app.title).toEqual('cursoAngularOpenWebinar');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cursoAmpliandoAngularOpenWebinars app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cursoAngularOpenWebinar app is running!');
   });
 });
