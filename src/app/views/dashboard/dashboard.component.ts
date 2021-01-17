9834269b9a465dd70a6bad28df2de6bded5b122b
ServerJon <ctsesiphon@hotmail.com>
2021-01-17 15:29:35 +0100

    tabla entradas añadida

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Entrada, Entradas } from 'src/app/shared/interfaces/entrada';
+import { EntradaService } from 'src/app/shared/services/entrada.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  // Atributo
+  public listadoEntradas: Entrada[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private entradaService: EntradaService) {
+    this.listadoEntradas = [];
   }
 
+  ngOnInit(): void {
+
+    this.listarEntradas();
+  }
+
+  private listarEntradas(): void {
+
+    this.entradaService.recuperarEntradas().subscribe(
+      (entradas: Entradas) => {
+        this.listadoEntradas = [...entradas.data];
+      }
+    )
+  }
 }
